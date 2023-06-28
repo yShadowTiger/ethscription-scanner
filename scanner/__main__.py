34d7e7059c0b9c01d178b364f6d2a9736836419c
yShadowTiger <TigerStudio@gmail.com>
2023-06-28 18:50:47 +0800

    fix: entry point

--- scanner/__main__.py
+++ scanner/__main__.py
@@ -1,69 +1,35 @@
-# -*- coding: utf-8 -*-
-# Author:   Alex Tang
-# Date:     2023/6/28
-"""
-__main__.py
-
-Feature:
-
-Scenario:
-
-"""
-import fcntl
-import os
+import signal
 import time
 
-import click as click
-import daemon
 from loguru import logger
 
 from scanner.chain import EthereumScanner
 from scanner.common import ConfigurationHelper
 
-lock_file = "/var/run/ethereum_service.lock"
+logger.add("service.log", rotation="1 day")
 
 
-# Check if the lock file exists
-def is_service_running():
-    try:
-        with open(lock_file, "w") as f:
-            fcntl.lockf(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
-    except IOError as ioe:
-        logger.error(ioe)
-        return True
-    return False
+class SignalHandler:
+    shutdown_requested = False
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.request_shutdown)
+        signal.signal(signal.SIGTERM, self.request_shutdown)
+
+    def request_shutdown(self, *args):
+        logger.info('Request to shutdown received, stopping')
+        self.shutdown_requested = True
+        ConfigurationHelper().dump()
+
+    def can_run(self):
+        return not self.shutdown_requested
 
 
-# Task processor
-def execute():
+signal_handler = SignalHandler()
+
+while signal_handler.can_run():
     EthereumScanner().process()
+    logger.info("Processing complete. Sleeping for 1 minute.")
+    time.sleep(ConfigurationHelper().get_interval())
 
-
-# Main function to run the daemon
-@click.command()
-@click.option('-c', '--config', type=click.Path(exists=True), help='Config file path')
-def main(config):
-    logger.add("service.log", rotation="1 day", level="INFO")
-
-    # Check if the service is already running
-    if is_service_running():
-        logger.error("Another instance of the service is already running. Exiting.")
-        return
-
-    with daemon.DaemonContext():
-        while True:
-            try:
-                logger.info("Processing unread blocks...")
-                execute()
-                time.sleep(ConfigurationHelper().get_inteval())
-            except Exception as e:
-                logger.exception(f"An error occurred: {str(e)}")
-                break
-
-            # Remove the lock file to release the lock
-            ConfigurationHelper().dump()
-            os.unlink(lock_file)
-
-
-if __name__ == "__main__":
-    main()
+logger.info("Shutting down, dump configuration")
